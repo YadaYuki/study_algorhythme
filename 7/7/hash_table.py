293dd4380ef93b43768753324347bced82365fe1
YadaYuki <yada.yuki@fuji.waseda.jp>
2020-02-29 21:13:54 +0900

    implement hash table search and insert function considered sinonimu

--- 7/7/hash_table.py
+++ 7/7/hash_table.py
@@ -4,19 +4,50 @@ class HashTable():
         self.array = [-1] * size
     
     def insert_value(self,value):
-        pos = value % self.size
-        # not same
-        if self.array[pos] != -1:
-            self.array[pos] = value
+        init_pos = value % self.size
+        pos = init_pos
+        # not already has num
+        while True:
+            if self.array[pos] == -1:
+                self.array[pos] = value
+                break
+            # move to next address 
+            pos += 1
+            # if pos over length of array , pos back to 0
+            pos = pos % self.size
+            # array is already full.
+            if pos == init_pos:
+                break
 
     def search_value(self,value):
-        pos = value % self.size
-        if self.array[pos] == value:
-            return pos
-        else:
-            return -1
+        init_pos = value % self.size
+        pos = init_pos
+        while True:
+            if self.array[pos] == value:
+                return pos
+            pos += 1
+            pos = pos % self.size
+            # value does not exist in array
+            if pos == init_pos:
+                break
+
 
 if __name__ == "__main__":
     ht = HashTable(10)
-    ht.insert_value(134)
-    print(ht.search_value(134))
+    # store number
+    while True:
+        print("Insert:",end="")
+        num = int(input())
+        if num < 0:
+            break
+        ht.insert_value(num)
+        print("inserted!")
+    # search number
+    while True:
+        print("Search:",end="")
+        num = int(input())
+        if num < 0:
+            break
+        print("{num} is in {p}".format(num=num,p=ht.search_value(num)))
+    print(ht.array)
+
