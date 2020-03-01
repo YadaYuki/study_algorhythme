a43abf24cb6ab07ec53d09612a04f6f4bae84658
YadaYuki <yada.yuki@fuji.waseda.jp>
2020-03-01 17:44:48 +0900

    revise knapsackDP function

--- 9/knapsackDP.py
+++ 9/knapsackDP.py
@@ -22,21 +22,28 @@ if __name__ == "__main__":
     ks = KnapSack()
     MAX_WEIGHT = 6
     ITEM_NUM = len(ks.item_arr)
-    value_arr = [[-1]* (MAX_WEIGHT + 1)] * ITEM_NUM
+    # value_arr = [[-1]* (MAX_WEIGHT + 1)] * ITEM_NUM
+    value_arr = [[-1 for i in range(MAX_WEIGHT + 1)] for j in range(ITEM_NUM)]
     value_arr[0] = [0,100,100,100,100,100,100]
     # calculate best value
     for i in range(1,ITEM_NUM):
         for j in range(MAX_WEIGHT + 1):
+            # if the weight(j) is larger than item weight → calculate value,
+            # if not → insert before i 
             if j >= ks.item_arr[i].weight:
+                # if j = 4,item weight = 2 → left_weight = 2
                 left_weight = j - ks.item_arr[i].weight
                 value = ks.item_arr[i].price + value_arr[i-1][left_weight]
                 if value >= value_arr[i-1][j]:
+                    print("{value}={item_price}+{prev_best}".format(value=value,item_price=ks.item_arr[i].price,prev_best=value_arr[i-1][left_weight]))
                     value_arr[i][j] = value
+                    print(value_arr)
                 else:
                     value_arr[i][j] = value_arr[i-1][j]
-            elif i == j == 0:
-                value_arr[i][j] = 0
+            # elif i == j == 0:
+            #     value_arr[i][j] = 0
             else:
                 value_arr[i][j] = value_arr[i-1][j]
+                
     print(value_arr)
 
